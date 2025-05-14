70ddc79088b7a846b17b44bd9a195f6ad73ef847
qwilgsi <orafan100@gmail.com>
2025-05-14 04:32:20 +0300

    Change staticfiles storage

--- TravelBlog/TravelBlog/settings.py
+++ TravelBlog/TravelBlog/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
